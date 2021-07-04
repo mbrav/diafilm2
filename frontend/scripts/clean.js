1308fe4c51f17790d4d5810f735ae71b9877c032
mbrav <mbrav@protonmail.com>
2021-07-05 00:53:37 +0300

    npm render scripts -> backend templates and static

--- frontend/scripts/clean.js
+++ frontend/scripts/clean.js
@@ -1,7 +1,7 @@
 const sh = require('shelljs');
 const upath = require('upath');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const destPath = upath.resolve(upath.dirname(__filename), '../../backend/diafilms/templates');
 
 sh.rm('-rf', `${destPath}/*`)
 
