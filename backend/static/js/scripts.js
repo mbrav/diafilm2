82b01e6f06b22550579ae07eeda78a0f72419687
mbrav <mbrav@protonmail.com>
2021-09-28 16:34:25 +0300

    fixed fullscreen 99%

--- backend/static/js/scripts.js
+++ backend/static/js/scripts.js
@@ -3,6 +3,54 @@
  * Copyright 2013-2021 mbrav
  * Licensed under MIT (https://github.com/StartBootstrap/dia-film2/blob/master/LICENSE)
  */
+
+var diafilmCarousel = document.getElementById("diafilm-carousel");
+
+function cleanSlideStyles() {
+	slides = document.querySelectorAll(".carousel-item > img");
+	for (let index = 0; index < slides.length; index++) {
+		slides[index].style.width = "100%";
+		slides[index].style.height = null;
+	}
+	return slides
+}
+
+w = window.innerWidth;
+h = window.innerHeight;
+window.addEventListener(
+	"resize",
+	function (event) {
+		w = window.innerWidth;
+		h = window.innerHeight;
+
+		if (document.fullscreenElement !== null) {
+			slides = cleanSlideStyles()
+
+			for (let index = 0; index < slides.length; index++) {
+				if (w > h) {
+					slides[index].style.height = h + "px";
+					slides[index].style.width = null;
+				} else {
+					slides[index].style.height = null;
+					slides[index].style.width = w + "px";
+				}
+			}
+		}
+	},
+	true
+);
+
+function diafilmFullScreenToggle() {
+	diafilmCarousel.requestFullscreen();
+}
+
+document.onfullscreenchange = function (event) {
+	cleanSlideStyles();
+};
+
+document.getElementById("full-screen-toggle").onclick = diafilmFullScreenToggle;
+
+
 window.addEventListener("DOMContentLoaded", () => {
 	let scrollPos = 0;
 	const mainNav = document.getElementById("mainNav");
@@ -30,10 +78,3 @@ window.addEventListener("DOMContentLoaded", () => {
 		scrollPos = currentTop;
 	});
 });
-
-function diafilmFullScreenToggle() {
-	var elem = document.getElementById("diafilm-carousel");
-	elem.requestFullscreen();
-}
-
-document.getElementById("full-screen-toggle").onclick = diafilmFullScreenToggle;
